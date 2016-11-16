5f8d201a7c46e6e6f3d3e6dac36f23514738c104
Greg Salvesen <greg@latenightdesigns.com>
2016-11-15 21:19:04 -0500

    Added functionality to reverse a string

--- 39 Reverse a String/main.cpp
+++ 39 Reverse a String/main.cpp
@@ -7,9 +7,21 @@
 //
 
 #include <iostream>
+#include <string>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Enter a string that you would like reversed: ";
+    
+    // Asks the user for a string, and creates a copy to be reversed
+    std::string initString = "";
+    std::cin >> initString;
+    std::string reverseString = initString;
+    
+    // Starting from the first character, swaps it with the last, and then moves one character in on either side, until they meet in the middle
+    for(int i = 0; i < reverseString.size() / 2; i++) {
+        std::swap(reverseString[i], reverseString[reverseString.size() - i - 1]);
+    }
+    
+    std::cout << initString << " reversed is " << reverseString << std::endl;
     return 0;
 }
